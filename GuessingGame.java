17f5d0ccaeb02d8554b913dd8b4b7c155b9de60b
Gryphon <91989335+whilegryph@users.noreply.github.com>
2022-01-20 21:11:05 -0800

    Implemented Donnelly's code in with mine.
    - Changed main to widen the scope of variable 'answerPlay' to use as a condition to continue the while loop.
    - Removed variable 'average' in results method, and instead type casted the divisor of the guessCount and gameCount variables.
    - Removed redundant code by removing unnecessary instances of guessCount.
    - Moved code that checked for type of win outside of the while loop, since any code that exists outside of it would mean the game has been won.

--- GuessingGame.java
+++ GuessingGame.java
@@ -20,21 +20,19 @@ public class GuessingGame {
       int best = 0;
       int gameCount = 0;
       int guessCount = 0;
+      String answerPlay;
       boolean a = true;
    
-      while(a) {
+      while(answerPlay.substring(0, 1).equalsIgnoreCase("y")) {
          Random rand = new Random();
          guessCount += playGame(rand, console);
          System.out.print("Do you want to play again? ");
-         String playAgain = console.next();
+         answerPlay = console.next();
          
          if(best > guessCount || best == 0) {
             best = guessCount;
          }
          gameCount++;
-         if(!playAgain.substring(0, 1).equalsIgnoreCase("y")) {
-            a = false;
-         }
       }
       
       results(gameCount, guessCount, best);
@@ -42,50 +40,48 @@ public class GuessingGame {
    }
    // Describes the rules of the game and how the game will be played to the user.
    public static void introduction() {
-      System.out.println(
-         "This program allows you to play a guessing game.\n" +
-         "I will think of a number between 1 and\n" +
-         MAX_NUM + "and will allow you to guess until\n" +
-         "you get it. For each guess, I will tell you\n" +
-         "whether the right answer is higher or lower\n" +
-         "than your guess.");
+      System.out.println("This program allows you to play a guessing game.");
+      System.out.println("I will think of a n number between 1 and");
+      System.out.println("100 and will allow you to guess until");
+      System.out.println("you get it. For each guess, I will tell you");
+      System.out.println("whether the right answer is higher or lower");
+      System.out.println("than your guess.");
    }
    // Plays the game with the user.
    public static int playGame(Random rand, Scanner console) {
       System.out.println("\nI'm thinking of a number between 1 and " + MAX_NUM + "...");
       
       int num = rand.nextInt(MAX_NUM) + 1;
-      
       int guessCount = 0;
       int randNum = rand.nextInt(MAX_NUM) + 1;
       int guess = 0;
        
       while (guess != randNum) {
+         
          System.out.print("Your guess? ");
          guess = console.nextInt();
+         guessCount++;
+         
          if (guess > randNum) {
             System.out.println("It's lower.");
-            guessCount++;
          } else if (guess < randNum) {
             System.out.println("It's higher.");
-            guessCount++;
-         } else {
-            if(guessCount == 0) {
-               System.out.println("You got it right in 1 guess.");
-            } else {
-               System.out.println("You got it right in " + guessCount + " guesses.");
-            }
          }
+      }
+      
+      if(guessCount == 1) {
+         System.out.println("You got it right in 1 guess.");
+      } else {
+         System.out.println("You got it right in " + guessCount + " guesses.");
       }
       return guessCount;
    }
    // Provides the stats of every game played to the user.
    public static void results(int gameCount, int guessCount, int best) {
-      double average = (double) guessCount/gameCount;
       System.out.println("\nOverall results:");
       System.out.println("    total games\t= " + gameCount);
       System.out.println("    total guesses = " + guessCount);
-      System.out.printf(("    guesses/game  = %.1f\n"), average);
+      System.out.printf(("    guesses/game  = %.1f\n"), (double) guessCount/gameCount);
       System.out.println("    best game\t\t= " + best);
    }
 }
